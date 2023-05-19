189d3fce0af9e0778bce6908b1fef6312786a591
leadang312 <lea.dang@y42.com>
2023-05-19 17:59:27 +0200

    Changes home page

--- Home.py
+++ Home.py
@@ -5,6 +5,17 @@ st.title("Home")
 "Hey there!"
 "You can use the following three applications to make your life easier when handling lead lists from Phantombuster, Zoominfo, and Cognism."
 
+"#### Required CSV Files"
+"You can extract the three required csv files here:"
+zo_url = "https://www.zoominfo.com/"
+st.write("[Zoominfo Profile Scraper](zo_url)")
+
+co_url = "https://www.cognism.com/de/"
+st.write("[Cognism Profile Scraper](co_url)")
+
+ph_url = "https://phantombuster.com/automations/sales-navigator/11108/sales-navigator-profile-scraper/"
+st.write("[Phantoombuster Profile Scraper](ph_url)")
+
 "#### List Merger"
 "Here, you can upload three CSV files obtained from Phantombuster, Zoominfo, or Cognism. The program will generate a merged file combining all the information from the three tables."
 
